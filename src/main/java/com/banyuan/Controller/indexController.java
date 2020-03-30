fefbd12be28a1d27ab2cd2c985a00ec990fdfce8
jay958718389@qq.com <Tj199602>
2020-03-30 17:52:23 +0800

    init

---
+++ src/main/java/com/banyuan/Controller/indexController.java
@@ -0,0 +1,13 @@
+package com.banyuan.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class indexController {
+    @GetMapping("/")
+    String getIndex(){
+        return "hello";
+    }
+}
+
