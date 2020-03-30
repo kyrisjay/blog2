e354a87d48a985cf9e92ab51d002a9fc17d98df4
jay958718389@qq.com <Tj199602>
2020-03-30 18:05:24 +0800

    init

--- src/main/java/com/banyuan/Controller/indexController.java
+++ src/main/java/com/banyuan/Controller/indexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class indexController {
     @GetMapping("/")
     String getIndex(){
-        return "hello";
+        return "about";
     }
 }
 
